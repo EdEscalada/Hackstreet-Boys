35f74acf974462b54f3fdc20dcf8d43d1c69e47b
EdEscalada <38880548+EdEscalada@users.noreply.github.com>
2025-11-06 20:53:01 +0800

    Delete src directory

--- src/auth.js
+++
@@ -1,7 +0,0 @@
-// src/auth.js
-import API from "./api";
-
-export const register = async (username, email, password) => { ... };
-export const login = async (username, password) => { ... };
-export const refreshToken = async () => { ... };
-export const getProfile = async () => { ... };
